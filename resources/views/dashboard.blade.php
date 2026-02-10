0019136061b5701bed40ffe31413ec1b13127ca3
SyawalLabs <waldevelopment06@gmail.com>
2026-02-10 22:43:26 +0800

    asda

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -83,7 +83,57 @@
             </div>
             <!-- [Mini Card] end -->
 
-
+            <!-- [Billable Time] start -->
+            <div class="col-xxl-6">
+                <div class="card stretch stretch-full">
+                    <div class="card-header">
+                        <h5 class="card-title">Billable Time</h5>
+                        <div class="card-header-action">
+                            <div class="card-header-btn">
+                                <div data-bs-toggle="tooltip" title="Delete">
+                                    <a href="javascript:void(0);" class="avatar-text avatar-xs bg-danger"
+                                        data-bs-toggle="remove"> </a>
+                                </div>
+                                <div data-bs-toggle="tooltip" title="Refresh">
+                                    <a href="javascript:void(0);" class="avatar-text avatar-xs bg-warning"
+                                        data-bs-toggle="refresh"> </a>
+                                </div>
+                                <div data-bs-toggle="tooltip" title="Maximize/Minimize">
+                                    <a href="javascript:void(0);" class="avatar-text avatar-xs bg-success"
+                                        data-bs-toggle="expand"> </a>
+                                </div>
+                            </div>
+                            <div class="dropdown">
+                                <a href="javascript:void(0);" class="avatar-text avatar-sm" data-bs-toggle="dropdown"
+                                    data-bs-offset="25, 25">
+                                    <div data-bs-toggle="tooltip" title="Options">
+                                        <i class="feather-more-vertical"></i>
+                                    </div>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-end">
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-at-sign"></i>New</a>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-calendar"></i>Event</a>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-bell"></i>Snoozed</a>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-trash-2"></i>Deleted</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-settings"></i>Settings</a>
+                                    <a href="javascript:void(0);" class="dropdown-item"><i
+                                            class="feather-life-buoy"></i>Tips & Tricks</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body custom-card-action">
+                        <div id="billable-time-bar-chart"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- [Billable Time] end -->
 
         </div>
         <!-- [ Footer ] start -->
@@ -111,40 +161,28 @@
 
                 var options = {
                     chart: {
-                        type: 'line',
-                        height: 250,
-                        toolbar: {
-                            show: false
-                        }
+                        type: 'bar',
+                        height: 300
                     },
                     series: [{
-                        name: 'Kasus',
+                        name: 'Jumlah',
+                        data: [25, 23, 20, 27]
                     }],
-                    xaxis: {},
-                    stroke: {
-                        curve: 'smooth',
-                        width: 3
+                    xaxis: {
+                        categories: ['7-A', '7-B', '7-C', '8-A']
                     },
-                    markers: {
-                        size: 4
-                    },
-                    colors: ['#dc3545'],
+                    colors: ['#0d6efd'],
                     dataLabels: {
                         enabled: false
-                    },
-                    grid: {
-                        strokeDashArray: 4
                     }
                 };
 
-                var chart = new ApexCharts(
-                    document.querySelector("#bounce-rate"),
-                    options
-                );
-
+                var chart = new ApexCharts(document.querySelector("#billable-time-bar-chart"), options);
                 chart.render();
+
             });
         </script>
+
 
     </main>
 
